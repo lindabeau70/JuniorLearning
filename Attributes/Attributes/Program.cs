63f85c43482fa8bb5e8adf4d0356bd95f6475ef3
Linda Beaugeard <linda@nickbeaugeard.net>
2021-07-12 11:55:42 +1000

    Done, done & done

---
+++ Attributes/Attributes/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+
+namespace Attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TypeInfo typeInfo = typeof(MyClass).GetTypeInfo();
+            Console.WriteLine($"The assembly qualified name of MyClass is {typeInfo.AssemblyQualifiedName}");
+
+            var attrs = typeInfo.GetCustomAttributes();
+            foreach(var attr in attrs)
+            {
+                Console.WriteLine("Attribute on MyClass: " + attr.GetType().Name);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Yep - example of Obsolete attribute on class
+    /// </summary>
+    [Obsolete("Example of making a clase obsolete.  Would give details of class that supersedes etc here...")]
+    public class MyClass
+    {
+
+    }
+
+    /// <summary>
+    /// Example of creating own custom attribute
+    /// </summary>
+    public class MySpecialAttribute : Attribute
+    {
+        public MySpecialAttribute(string comment)
+        {
+            // do something
+        }
+    }
+
+    /// <summary>
+    /// Example of custom attribute applied to class
+    /// </summary>
+    [MySpecial("Here we apply the attribute created to another class")]
+    public class SomeOtherClass
+    {
+
+    }
+}
